87434bac30cdd3acfabb3330d81e4eb27b8704dc
mc <46639858+michengs@users.noreply.github.com>
2019-01-13 15:11:58 +0800

    Update potions.js

--- potions.js
+++ potions.js
@@ -1,14 +1,16 @@
 module.exports = [
     /**
-     * IF YOU WANT TO ADD MORE POTS, JUST COPY THE CONTENT BETWEEN { }, 
-     * AND CHANGE ONLY THE ITEMS LISTED ABOVE:
+     * If you want to add more pots, just copy the content between { },
+     * And change only these items:
      * 
-     * ITEM - CODE OF THE ITEM, CAN BE FOUND USING IN-BUILT COMMAND OR TERA DATABASE
-     * CD - ITEM COOLDOWN TIME IN SECONDS
-     * HP - TRUE: HP POTION / FALSE: MP POTION
-     * USE_AT - HOW MUCH HP/MP TO BE USED (IF 50 THE POTION WILL BE USED IF YOU HAVE LESS THAN 50% HP/MP)
+     * item     - Item id, can be found using built-in command or in tera datacenter... or here: https://teralore.com/
+     * name     - Anything to remind you which potion is this, won't be used in code.
+     * cd       - Item cooldown in seconds.
+     * hp       - True if hp potion | False if mp potion.
+     * use_at   - At which percentage you want the hp or mp pot to be used at. (e.g. if 50, the potion will be used if you have less than 50% of your hp or mp.
+     * slaying  - Same as use_at, but only in slaying mode. (This will be ignored if it's mp potion).
      * 
-     * JUST COPY THE OTHER VALUES
+     * copy the rest of values without changing.
      */
     // HP
     {
@@ -17,8 +19,8 @@ module.exports = [
         cd: 10,
         hp: true,
         use_at: 50,
+        slaying: 30,
         inCd: false,
-        id: 0,
         invQtd: 0
     },
     {
@@ -27,8 +29,8 @@ module.exports = [
         cd: 30,
         hp: true,
         use_at: 20,
+        slaying: -25,
         inCd: false,
-        id: 0,
         invQtd: 0
     },
     {
@@ -37,20 +39,49 @@ module.exports = [
         cd: 30,
         hp: true,
         use_at: 20,
+        slaying: -25,
         inCd: false,
-        id: 0,
         invQtd: 0
     },
-
+    {
+        item: 111,
+        name: "Rejuvenation Potion",
+        cd: 30,
+        hp: true,
+        use_at: 15,
+        slaying: -70,
+        inCd: false,
+        invQtd: 0
+    },
+    {
+        item: 112,
+        name: "Rejuvenation Potion",
+        cd: 30,
+        hp: true,
+        use_at: 15,
+        slaying: -70,
+        inCd: false,
+        invQtd: 0
+    },
     // MP
     {
         item: 6562,
         name: "Prime Replenishment Potable",
         cd: 10,
         hp: false,
-        use_at: 50,
+        use_at: 55,
+        slaying: 0,
         inCd: false,
-        id: 0,
+        invQtd: 0
+    },
+    {
+        item: 130,
+        name: "Divine Infusion",
+        cd: 30,
+        hp: false,
+        use_at: 20,
+        slaying: 0,
+        inCd: false,
         invQtd: 0
     },
 ]
